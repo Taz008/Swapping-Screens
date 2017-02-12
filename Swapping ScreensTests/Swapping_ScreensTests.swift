c88e6bac7eb1ca5f6db535c0382cfd8866b50830
hemang sharma <hemangsharma@hemangs-MacBook-Pro.local>
2017-02-12 11:58:42 -0500

    Initial Commit

---
+++ Swapping ScreensTests/Swapping_ScreensTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swapping_ScreensTests.swift
+//  Swapping ScreensTests
+//
+//  Created by hemang sharma on 2/12/17.
+//  Copyright © 2017 hemang sharma. All rights reserved.
+//
+
+import XCTest
+@testable import Swapping_Screens
+
+class Swapping_ScreensTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
